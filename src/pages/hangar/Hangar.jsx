21f414687ee7189ba7d48da447a323c47ca3d3a6
ToddGallegos <toroga@hotmail.com>
2023-03-09 06:45:29 -0800

    store/hangar design implemented

--- src/pages/hangar/Hangar.jsx
+++ src/pages/hangar/Hangar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import img from "../../images/152.png";
+import "./hangar.css";
 
 export default function Hangar() {
   return (
-    <div>Hangar</div>
+    <div className="hangarContainer">
+        <h1 className="hangarTitle">Your Aircraft</h1>
+        <div className="card">
+            <img className="aircraftImage" src={img} alt="plane"/>
+            <p className="hangarParagraph">Price: $64,000</p>
+            <p className="hangarParagraph">MaxLoad: 502 lbs</p>
+            <div className="cardButtonDiv">
+                <button className="hangarSellButton">Sell</button>
+            </div>
+        </div>
+    </div>
   )
 }
--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import img from "../../images/152.png";
+import img1 from "../../images/172.png";
+import img2 from "../../images/ctsl.png";
+import img3 from "../../images/xcub.png";
+import "./store.css";
 
 export default function Store() {
-  return (
-    <div>Store</div>
-  )
+    return (
+        <div className="storeContainer">
+
+            <h1 className="storeTitle">Store</h1>
+
+            <div className="cardContainer">
+
+                <div className="card">
+                    <img className="aircraftImage" src={img} alt="plane" />
+                    <p className="storeParagraph">Price: $64,000</p>
+                    <p className="storeParagraph">MaxLoad: 502 lbs</p>
+                    <div className="cardButtonDiv">
+                        <p className="storeParagraph">Purchased</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img className="aircraftImage" src={img2} alt="plane" />
+                    <p className="storeParagraph">Price: $140,000</p>
+                    <p className="storeParagraph">MaxLoad: 283 lbs</p>
+                    <div className="cardButtonDiv">
+                        <button className="storeBuyButton">Buy</button>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img className="aircraftImage" src={img1} alt="plane" />
+                    <p className="storeParagraph">Price: $200,000</p>
+                    <p className="storeParagraph">MaxLoad: 679 lbs</p>
+                    <div className="cardButtonDiv">
+                        <button className="storeBuyButton">Buy</button>
+                    </div>
+                </div>
+                
+                <div className="card">
+                    <img className="aircraftImage" src={img3} alt="plane" />
+                    <p className="storeParagraph">Price: $325,000</p>
+                    <p className="storeParagraph">MaxLoad: 930 lbs</p>
+                    <div className="cardButtonDiv">
+                        <button className="storeBuyButton">Buy</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
